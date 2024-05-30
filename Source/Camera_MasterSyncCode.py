65e7acdb16b65249b26e0992661727327efb9e68
Matt Boillat <8787925@gmail.com>
2024-05-29 22:40:13 -0700

    Added local camera configuration (master camera) to master script.
    Captures multiple frames from slave script

--- Source/Camera_MasterSyncCode.py
+++ Source/Camera_MasterSyncCode.py
@@ -2,9 +2,12 @@ import asyncio
 import websockets
 import json
 import numpy as np
-import io
+import json
+from picamera2 import Picamera2, Preview
+import time
 import cv2
-import time 
+from Libraries import PiRAW2TIF_16bit
+import io
 
 #this code is meant to be ran on the computer who is running the 'Trigger' camera
 #
@@ -17,8 +20,18 @@ WEBSOCKET_MASTER = 'camera0bee.lan'
 WEBSOCKET_SLAVE = 'beemonitor.lan'
 WEBSOCKET_PORT = 18873
 FRAGMENT_SIZE = 1 #indicies
+NUMBER_OF_FRAMES = 10
+
+picam2a = Picamera2()
+camera_configa = picam2a.create_still_configuration(
+        main={"size": (1920, 1080)},
+        queue = False)
+picam2a.configure(camera_configa)
+picam2a.set_controls({"ExposureTime": 10000, "AnalogueGain": 5})
+
 
 async def client():
+    global picam2a
     async with websockets.connect("ws://" + WEBSOCKET_SLAVE + ":" + str(WEBSOCKET_PORT)) as websocket:
         # Send command to execute function
         command = {'action': 'start_camera'}
@@ -27,25 +40,34 @@ async def client():
         startResult = await websocket.recv()
         startResult = json.loads(startResult)
         if (startResult['result'] == 'success'): 
-            #good to capture
-            command['action'] = 'capture'
-            await websocket.send(json.dumps(command))
+            #good to capture locally
+            time.sleep(1)
+            picam2a.start(show_preview=False)
 
-            full_data = bytearray()
-            while True:
-                chunk = await websocket.recv()
-                if chunk == b"END":
-                    break
-                full_data.extend(chunk)
-                await websocket.send("Ok")
-            
-            # Deserialize the binary data back into a NumPy array
-            buffer = io.BytesIO(full_data)
-            buffer.seek(0)
-            large_array = np.load(buffer)
-            cv2.imwrite('testImage.jpg', large_array)
-            print(f"Received array shape: {large_array.shape}")
+            for i in range(NUMBER_OF_FRAMES):
+                localImage = picam2a.capture_array()
+                command['action'] = 'capture'
 
+                #capture remotely
+                await websocket.send(json.dumps(command))
+
+                full_data = bytearray()
+                while True:
+                    chunk = await websocket.recv()
+                    if chunk == b"END":
+                        break
+                    full_data.extend(chunk)
+                    await websocket.send("Ok")
+                
+                # Deserialize the binary data back into a NumPy array
+                buffer = io.BytesIO(full_data)
+                buffer.seek(0)
+                large_array = np.load(buffer)
+                cv2.imwrite('testImage' + str(i) + '.jpg', large_array)
+                cv2.imwrite('localImageTest' + str(i) + '.jpg', localImage)
+                print(f"Received array shape: {large_array.shape}")
+            print('Done')
+            exit()
             #large_data = b"".join(full_data)
             #captureResult = await websocket.recv()
             #captureResult = np.array(json.loads(captureResult))
